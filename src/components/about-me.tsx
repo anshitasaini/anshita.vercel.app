5e1c9177714f45f5e3185deee23c168b68d59839
anshita <anshita@openai.com>
2025-02-17 15:01:18 -0800

    fix build

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -16,26 +16,26 @@ export default function AboutMe() {
           />
         </div>
         <div className="flex flex-col space-y-4 text-sm md:text-base lg:text-lg xl:text-xl">
-          <p className="text-3xl font-bold mb-3">hi, i'm anshita!</p>
+          <p className="text-3xl font-bold mb-3">hi, i&apos;m anshita!</p>
           <p>
             i&apos;m an{" "}
             <b>applied AI engineer, inclusive AI advocate, and speaker.</b>
           </p>
           <p>
-            currently, i'm a member of technical staff at OpenAI on ChatGPT
-            Growth. i'm also the founder of Wiser AI, an initiative to empower
-            marginalized genders in computer science in shaping ethical and
-            responsible AI.
+            currently, i&apos;m a member of technical staff at OpenAI on ChatGPT
+            Growth. i&apos;m also the founder of Wiser AI, an initiative to
+            empower marginalized genders in computer science in shaping ethical
+            and responsible AI.
           </p>
           <p>
-            i've previously founded and led various initiatives for inclusive
-            tech, including Code4Care and She Codes Art, impacting over 2,000
-            students around the world through 100+ computer science workshops
-            and conferences.
+            i&apos;ve previously founded and led various initiatives for
+            inclusive tech, including Code4Care and She Codes Art, impacting
+            over 2,000 students around the world through 100+ computer science
+            workshops and conferences.
           </p>
           <p>
-            in the past year, i've spoken at summits, podcasts, and tech events
-            reaching over 20,000 people, including being featured on the{" "}
+            in the past year, i&apos;ve spoken at summits, podcasts, and tech
+            events reaching over 20,000 people, including being featured on the{" "}
             <a
               href="https://blogs.nvidia.com/blog/technovation-inclusive-ai/"
               target="_blank"
@@ -58,7 +58,8 @@ export default function AboutMe() {
           <p>
             want to chat about speaking opportunities or collaborating on
             inclusive AI initiatives? reach out at{" "}
-            <i>anshitasaini [at] gmail [dot] com </i>{":)"}
+            <i>anshitasaini [at] gmail [dot] com </i>
+            {":)"}
           </p>
         </div>
       </div>
